3218d5e49f3be64f3f1390d16e84b8e0987aaedd
Noob Science <78465651+newtoallofthis123@users.noreply.github.com>
2022-07-25 23:45:58 +0530

    Fix Blog Link and Remove Notifications

--- brain.js
+++ brain.js
@@ -6,33 +6,3 @@ window.onload = function() {
         speed: '1',
     });
     }; 
-Toastify({
-    text: "Follow Me on Twitter for Latest Updates",
-    duration: 30000,
-    destination: "https://twitter.com/noobscience1",
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    avatar: "assets/images/twitter.svg",
-    style: {
-        background: "linear-gradient(to right, #0e2a47, #0e2a36",
-    },
-    onClick: function(){} // Callback after click
-}).showToast();
-Toastify({
-    text: "Support Me by Forking my Projects on GitHub",
-    duration: 30000,
-    destination: "https://github.com/newtoallofthis123",
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    avatar: "assets/images/github.svg",
-    style: {
-        background: "linear-gradient(to right, #0e2a47, #0e2a36",
-    },
-    onClick: function(){} // Callback after click
-}).showToast();
